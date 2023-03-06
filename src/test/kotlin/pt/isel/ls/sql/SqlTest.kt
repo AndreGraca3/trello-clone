8b10a309b143a9291603b4d75a080dbc8c35bf18
LordKoff-cmd <a46052@alunos.isel.pt>
2023-03-06 21:31:39 +0000

    SqlTest done

--- src/test/kotlin/pt/isel/ls/sql/SqlTest.kt
+++ src/test/kotlin/pt/isel/ls/sql/SqlTest.kt
@@ -20,8 +20,8 @@ class SqlTest {
         fun setup()  {
             dataSource = PGSimpleDataSource()
             val jdbcDatabaseURL = System.getenv("JDBC_DATABASE_URL")
-            //dataSource.setURL(jdbcDatabaseURL)
-            dataSource.setURL("jdbc:postgresql://localhost:5432/?user=postgres&password=isel")
+            dataSource.setURL(jdbcDatabaseURL)
+            //dataSource.setURL("jdbc:postgresql://localhost:5432/?user=postgres&password=isel")
         }
 
     }
@@ -29,7 +29,7 @@ class SqlTest {
     @BeforeTest
     fun testConnection(){
         assertNotNull(dataSource)
-        //assert<Throwable> { dataSource.getConnection() }
+        //assert<Throwable> { dataSource.getConnection() } //TODO PSQLException(pwd error)
     }
 
     @Test
@@ -39,30 +39,40 @@ class SqlTest {
             "delete from dbo.courses;"
 
         val cmdUpdate =
-            "insert into dbo.courses(name) values ('LEIM');\n" +
+            "insert into dbo.courses(cid, name) values (1,'LEIM');\n" +
             "insert into dbo.students(course, number, name) values (1, 12345, 'Jorge');\n" +
             "insert into dbo.students(course, number, name) select cid as course, 12346 as number, 'Bob' as name from dbo.courses where name = 'LEIM';"
 
         dataSource.getConnection().use {
+            it.autoCommit = false  //needed for test effects
+
+            /** Delete students and courses **/
             val stmDelete = it.prepareStatement(cmdDelete)
             stmDelete.executeUpdate()
 
+            /** Update students and courses **/
             val stmUpdate = it.prepareStatement(cmdUpdate)
             stmUpdate.executeUpdate()
-            val stmSelectS = it.prepareStatement("select * from dbo.students where name='Jorge'")
-            val stmSelectC = it.prepareStatement("select * from dbo.courses where cid=1")
 
+            /** Select students **/
+            val stmSelectS = it.prepareStatement("select * from dbo.students where name='Jorge'")
             val rsStudents = stmSelectS.executeQuery()
+
+            /** Select courses **/
+            val stmSelectC = it.prepareStatement("select * from dbo.courses where cid=1")
             val rsCourses = stmSelectC.executeQuery()
 
+            /** Iterate students ResultSet **/
             while (rsStudents.next()) {
                 assertEquals(rsStudents.getInt("number"), 12345)
                 assertEquals(rsStudents.getString("name"), "Jorge")
                 assertEquals(rsStudents.getInt("course"), 1)
             }
+
+            /** Iterate courses ResultSet **/
             while (rsCourses.next()){
-                assertEquals(rsStudents.getInt("cid"), 1)
-                assertEquals(rsStudents.getString("name"), "LEIM")
+                assertEquals(rsCourses.getInt("cid"), 1)
+                assertEquals(rsCourses.getString("name"), "LEIM")
             }
             it.prepareStatement("ROLLBACK;").executeUpdate()
         }
