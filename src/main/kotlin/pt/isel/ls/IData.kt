51fced40ab212eccf30fa246f07464ca27b1d55f
LordKoff-cmd <a46052@alunos.isel.pt>
2023-03-12 15:19:07 +0000

    IData (Interface) completed and Types and DataStructures (Users,Boards,BoardList and Card) done

---
+++ src/main/kotlin/pt/isel/ls/IData.kt
@@ -0,0 +1,53 @@
+package pt.isel.ls
+
+import java.util.Date
+import kotlin.collections.*
+
+interface IData {
+
+    /** ----------------------------
+     *  User Functions
+     *  ------------------------------**/
+
+    fun createUser(name: String, email: String): Pair<String,Int>
+
+    fun getUserInfo(idUser: Int): User?
+
+    /** ----------------------------
+     *  Board Functions
+     *  ------------------------------**/
+
+    fun createBoard(name: String, description: String): Int
+
+    fun addUserToBoard(idUser: Int, idBoard: Int): Boolean //Int? (idBoard) ou Board?
+
+    fun getBoardsFromUser(idUser: Int): List<Board>
+
+    fun getBoardInfo(idBoard: Int): Board
+
+    /** ----------------------------
+     *  List Functions
+     *  ------------------------------**/
+
+    fun createNewListInBoard(name: String): Int
+
+    fun getListOfBoard(idBoard: Int): List<BoardList>
+
+    fun getListInfo(idBoard: Int, idList: Int): BoardList
+
+    /** ----------------------------
+     *  Card Functions
+     *  ------------------------------**/
+
+    fun createCard(name: String, description: String, endDate: Date): Int //check endDate
+
+    fun createCard(name: String, description: String): Int
+
+    fun getCardFromList(idBoard: Int, idList: Int): List<Card>
+
+    fun getCardInfoFromList(idBoard: Int, idList: Int, idCard: Int): Card
+
+    fun moveCard(idList: Int): Boolean
+
+
+}
--- src/main/kotlin/pt/isel/ls/tasksDataMem.kt
+++ src/main/kotlin/pt/isel/ls/tasksDataMem.kt
@@ -1,10 +1,21 @@
 package pt.isel.ls
 import kotlinx.serialization.Serializable
+import java.util.Date
+
 @Serializable
 data class User(val idUser : Int = 0 , val email : String, val name : String, val token : String = "")
+@Serializable
+data class Board(val idUser : Int = 0, val idBoard : Int = 0 , val description : String, val name : String)
+@Serializable
+data class BoardList(val idBoard : Int = 0, val idList : Int = 0, val phase : String, val name : String)
+@Serializable
+data class Card(val idList : Int = 0, val idCard: Int = 0, val name: String, val description: String, val startDate: Date, val endDate: Date, val archived: Boolean)
 
 val users = mutableListOf<User>()
 val nextId = 0
+val boards = mutableListOf<Board>()
+val boardList = mutableListOf<BoardList>()
+val cards = mutableListOf<Card>()
 
 fun addUser(user : User){
     users.add(user)
