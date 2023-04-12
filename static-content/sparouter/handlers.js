8e0ce29e1010addf0c95b8461689491435d92c6a
LordKoff-cmd <a46052@alunos.isel.pt>
2023-04-12 02:38:02 +0100

    HTML START

--- static-content/sparouter/handlers.js
+++ static-content/sparouter/handlers.js
@@ -2,14 +2,20 @@
 const BASE_URL = "http://localhost:8080/"
 
 function getHome(mainContent) {
-    const h1 = document.createElement("h1")
-    const text = document.createTextNode("home Page")
-    h1.replaceChildren(text)
-    mainContent.replaceChildren(h1)
+    const h3 = document.createElement("h3")
+    const text = document.createTextNode("This is OurTrello App")
+    h3.replaceChildren(text)
+    mainContent.replaceChildren(h3)
 }
+function getUser(mainContent, token) {
+    const headers = new Headers({
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': 'application/json'
+    });
 
-function getUser(mainContent) {
-    fetch(BASE_URL + "user")
+    fetch(BASE_URL + "user", {
+        headers: headers
+    })
         .then(res => res.json())
         .then(user => {
             console.log(user)
@@ -18,9 +24,112 @@ function getUser(mainContent) {
         })
 }
 
+function getSignup(mainContent) {
+    const signupBox = document.createElement("div");
+    signupBox.classList.add("signup-box");
+    signupBox.innerHTML = `
+    <h2 style="text-align:center;">User Creation</h2>
+    <form id="signup-form" style="display: flex; flex-direction: column; align-items: center;">
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name">
+
+      <label for="email">Email:</label>
+      <input type="text" id="email" name="email">
+
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password">
+
+      <input type="submit" value="Sign Up">
+    </form>
+  `;
+    mainContent.replaceChildren(signupBox);
+
+    const form = document.getElementById("signup-form");
+    form.addEventListener("submit", (event) => {
+        event.preventDefault(); // prevent form from submitting and refreshing the page
+        const formData = new FormData(form);
+        const name = formData.get("name");
+        const email = formData.get("email");
+        const password = formData.get("password");
+        createUser(name, email, password);
+    });
+}
+
+function createUser(name, email, password) {
+    const data = { name, email };
+    fetch(BASE_URL + "user", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    })
+        .then((res) => res.json())
+        .then((response) => {
+            const { id, token } = response;
+            console.log(`New user created with ID ${id} and token ${token}`);
+
+            // redirect to the user's page after successful sign-up
+            window.location.hash = "#user";
+            handlers.getUser(document.getElementById("mainContent"), token);
+        })
+        .catch((err) => {
+            console.error("Error creating user:", err);
+        });
+}
+
+
+function getLogin(mainContent) {
+    const loginBox = document.createElement("div");
+    loginBox.classList.add("login-box");
+    loginBox.innerHTML = `
+    <h2 style="text-align:center;">Login</h2>
+    <form id="login-form" style="display:flex;flex-direction:column;align-items:center;">
+      <label for="email">Email:</label>
+      <input type="text" id="email" name="email">
+
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password">
+
+      <input type="submit" value="Log In" style="margin-top: 1rem;">
+    </form>
+  `;
+    mainContent.replaceChildren(loginBox);
+
+    const loginForm = document.getElementById("login-form");
+    loginForm.addEventListener("submit", async (event) => {
+        //event.preventDefault();
+        TODO("Implement login functionality")
+        const formData = new FormData(loginForm);
+        const email = formData.get("email");
+        const password = formData.get("password");
+
+        // Send a request to the server to authenticate the user
+        const response = await fetch(BASE_URL + "user", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, password }),
+        });
+
+        if (response.ok) {
+            // Authentication successful, redirect to the #user page
+            window.location.href = "#user";
+        } else {
+            // Authentication failed, show an error message
+            const errorBox = document.createElement("div");
+            errorBox.textContent = "Incorrect email or password";
+            loginForm.appendChild(errorBox);
+        }
+    });
+}
+
+
+
 export const handlers = {
     getHome,
-    getUser
+    getUser,
+    getLogin,
+    getSignup
 }
 
 export default handlers
--- static-content/sparouter/index.js
+++ static-content/sparouter/index.js
@@ -7,16 +7,26 @@ window.addEventListener('hashchange',hashChangeHandler)
 function loadHandler() {
     router.addRouteHandler("home",handlers.getHome)
     router.addRouteHandler("user",handlers.getUser)
+    router.addRouteHandler("login",handlers.getLogin)
+    router.addRouteHandler("signup",handlers.getSignup)
 
     hashChangeHandler()
 }
 
 function hashChangeHandler() {
-
     const mainContent = document.getElementById("mainContent")
     const path = window.location.hash.replace("#","")
 
     const handler = router.getRouteHandler(path)
 
-    handler(mainContent)
+    if (path === "user") {
+        const token = localStorage.getItem('token')
+        if (token) {
+            handler(mainContent, token)
+        } else {
+            window.location.hash = "login"
+        }
+    } else {
+        handler(mainContent)
+    }
 }
